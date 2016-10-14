3e6717c48092cc9df1193c30962c76cecfa6ca56
Mariya2 <chery_vi@abv.bg>
2016-10-14 16:52:25 +0300

    test

--- server/login.php
+++ server/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-
+//jeij
 if(isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
 	$_POST = array_merge($_POST, (array) json_decode(trim(file_get_contents('php://input')), true));
 }
